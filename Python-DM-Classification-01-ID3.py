c1209c7eb462ca79e588a50aae2e0a29fab2a719
Valdecy Pereira <valdecypereira@yahoo.com.br>
2017-07-27 09:06:41 -0300

    Update Python-DM-Classification-01-ID3.py

--- Python-DM-Classification-01-ID3.py
+++ Python-DM-Classification-01-ID3.py
@@ -7,8 +7,7 @@
 # Lesson: Decision Trees - ID3
 
 # Citation: 
-# PEREIRA, V. (2018). Project: ID3, File: Python-DM-Classification-01-ID3.py, GitHub repository: 
-# <https://github.com/Valdecy/Iterative-Dichotomiser-3>
+# PEREIRA, V. (2018). Project: ID3, File: Python-DM-Classification-01-ID3.py, GitHub repository: <https://github.com/Valdecy/ID3>
 
 ############################################################################
 
@@ -41,19 +40,11 @@ def dt_id3(Xdata, ydata):
     label = label.reshape(1, len(uniqueWords[0]))
     
     ################    Part 2 - Initialization    #############################
-    # ID3 - Dataset Entropy
-    entropy = 0
-    denominator_1 = dataset.shape[0]
-    for i in range(0, label.shape[1]):
-        numerator = (dataset[(dataset.iloc[:,0] == label[0,i])].count())[0]
-        entropy = entropy - (numerator/denominator_1)* np.log2((numerator/denominator_1))
-    
-    # ID3 - initializing Variables
+    # ID3 - Initializing Variables
     i = 0
     branch = [None]*1
     branch[0] = dataset
     gain = np.empty([1, branch[i].shape[1]])
-    gain.fill(entropy)
     rule = [None]*1
     rule[0] = "IF "
     root_index = 0
@@ -63,21 +54,26 @@ def dt_id3(Xdata, ydata):
     ################     Part 3 - id3 Algorithm    #############################
     # ID3 - Algorithm
     while (i < stop):
-        for fill in range(0, gain.shape[1]):
-            gain[0, fill] = entropy
+        entropy = 0
+        denominator_1 = branch[i].shape[0]
+        for entp in range(0, label.shape[1]):
+            numerator = (branch[i][(branch[i].iloc[:,0] == label[0, entp])].count())[0]
+            if numerator > 0:
+                entropy = entropy - (numerator/denominator_1)* np.log2((numerator/denominator_1))
+        gain.fill(entropy)
         for element in range(1, branch[i].shape[1]):
-            if len(np.unique(branch[i][0])) == 1 or len(branch[i]) == 1 or gain.sum(axis=1) - gain[0,0] == -(gain.shape[1] - 1):
+            if len(np.unique(branch[i][0])) == 1 or len(branch[i]) == 1 or gain.sum(axis=1) == 0:
                  rule[i] = rule[i] + " THEN " + name + " = " + branch[i].iloc[0, 0] + "."
                  rule[i] = rule[i].replace(" AND  THEN ", " THEN ")
                  skip_update = True
                  break
-            if gain[0, element] != -1.0:
-                for word in range(0, len(uniqueWords[element])):
-                    for lbl in range(0, label.shape[1]):
-                        numerator = (branch[i][(branch[i].iloc[:,0] == label[0, lbl]) & (branch[i].iloc[:, element] == uniqueWords[element][word])].count())[0]
-                        denominator_2 = (branch[i][(branch[i].iloc[:, element] == uniqueWords[element][word])].count())[0]
-                        if numerator > 0:
-                            gain[0, element] = gain[0, element] + (denominator_2/denominator_1)* (numerator/denominator_2)* np.log2((numerator/denominator_2))
+            #if gain[0, element] != -1.0:
+            for word in range(0, len(uniqueWords[element])):
+                denominator_2 = (branch[i][(branch[i].iloc[:, element] == uniqueWords[element][word])].count())[0]
+                for lbl in range(0, label.shape[1]):
+                    numerator = (branch[i][(branch[i].iloc[:,0] == label[0, lbl]) & (branch[i].iloc[:, element] == uniqueWords[element][word])].count())[0]
+                    if numerator > 0:
+                        gain[0, element] = gain[0, element] + (denominator_2/denominator_1)* (numerator/denominator_2)* np.log2((numerator/denominator_2))
     
         if skip_update == False:
             gain[0,0] = -gain[0,0]
@@ -88,15 +84,16 @@ def dt_id3(Xdata, ydata):
             
             for word in range(0, len(uniqueWords[root_index])):
                 branch.append(branch[i][branch[i].iloc[:, root_index] == uniqueWords[root_index][word]])
-                rule.append(rule[i] + " = " + "(" + uniqueWords[root_index][word] + ")")
+                rule.append(rule[i] + " = " + "{" + uniqueWords[root_index][word] + "}")
             
             for logic_connection in range(1, len(rule)):
-                if len(np.unique(branch[i][0])) != 1 and rule[logic_connection].endswith(" AND ") == False  and rule[logic_connection].endswith(")") == True:
+                if len(np.unique(branch[i][0])) != 1 and rule[logic_connection].endswith(" AND ") == False  and rule[logic_connection].endswith("}") == True:
                     rule[logic_connection] = rule[logic_connection] + " AND "
         skip_update = False
         i = i + 1
         stop = len(rule)
     
+    rule.append("IF No Rule Apply THEN " + name + " = " + dataset.agg(lambda x:x.value_counts().index[0])[0] + ".")
     for i in range(len(rule) - 1, -1, -1):
         if rule[i].endswith(".") == False:
             del rule[i]
@@ -109,8 +106,8 @@ def dt_id3(Xdata, ydata):
 
 df = pd.read_csv('Python-DM-Classification-01-ID3.csv', sep = ';')
 
-X = df.iloc[:, 0:4]
-y = df.iloc[:, 4]
+X = df.iloc[:, 0:4] # Attributes
+y = df.iloc[:, 4]   # Target
 
 dt_id3(Xdata = X, ydata = y)
 
