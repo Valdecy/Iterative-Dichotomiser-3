a34e6f66a8d731e487d197df41b703c08507d15c
Valdecy Pereira <valdecypereira@yahoo.com.br>
2017-08-29 18:35:18 -0300

    Update Python-DM-Classification-03-ID3.py

--- Python-DM-Classification-03-ID3.py
+++ Python-DM-Classification-03-ID3.py
@@ -18,6 +18,8 @@ from scipy import stats
 
 # Function: Performs a Chi_Squared test or Fisher Exact test  
 def chi_squared_test(label_df, feature_df):
+    label_df.reset_index(drop=True, inplace=True)
+    feature_df.reset_index(drop=True, inplace=True)
     data = pd.concat([pd.DataFrame(label_df.values.reshape((label_df.shape[0], 1))), feature_df], axis = 1)
     data.columns=["label", "feature"]
     contigency_table = pd.crosstab(data.iloc[:,0], data.iloc[:,1], margins = False)
